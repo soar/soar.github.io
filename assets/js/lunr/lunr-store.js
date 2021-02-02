ef4944cd5e8d3cc8614a0e2caf6a02d6c8d2d444
soar <soar@users.noreply.github.com>
2021-02-02 21:25:29 +0000

    deploy: soar/soarname-mirror@2bd918aee1e950c21ff4b9b0506c21e8543b75fb

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Test2!",
+        "excerpt":"This is test2 post  ","categories": ["jekyll","update"],
+        "tags": ["test1","test2"],
+        "url": "/jekyll/update/2021/02/02/test2.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": ["test1","test2"],
+        "url": "/jekyll/update/2021/02/02/welcome-to-jekyll.html",
+        "teaser": null
+      }]
